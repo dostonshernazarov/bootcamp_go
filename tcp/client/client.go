a7bd198a46825142820249bb928abfef8a838837
doston <dostonshernazarov2001@gmail.com>
2024-01-18 21:45:13 +0500

    Created client server

--- tcp/client/client.go
+++ tcp/client/client.go
@@ -2,45 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
-	"time"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+
+	conn, err := net.Dial("tcp", "127.0.0.1:8081")
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Println(err)
 	}
 	defer conn.Close()
-	for {
-		var source string
-		fmt.Print("Введите слово: ")
-		_, err := fmt.Scan(&source)
-		if err != nil {
-			fmt.Println("Некорректный ввод", err)
-			continue
-		}
-		// отправляем сообщение
-		if n, err := conn.Write([]byte(source)); n == 0 || err != nil {
-			fmt.Println(err)
-			return
-		}
-		// получим ответ
-		fmt.Println("Ответ:")
-		conn.SetReadDeadline(time.Now().Add(time.Second * 5))
 
-		// Получение данных вынесено в отдельный цикл,
-		// поэтому даже если сервер получит больше
-		// много данных, он их обработает
-		for {
-			buff := make([]byte, 1024)
-			n, err := conn.Read(buff)
-			if err != nil {
-				break
-			}
-			fmt.Print(string(buff[0:n]))
-			conn.SetReadDeadline(time.Now().Add(time.Millisecond * 700))
-		}
+	message := make([]byte, 1024)
+	n, err := conn.Read(message)
+	if err != nil {
+		log.Println(err)
 	}
+	fmt.Println(string(message[:n]))
 }
